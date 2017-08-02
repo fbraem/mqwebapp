a273c4772e6b810b3efdb52fe61ce456d3ba2638
fbraem <franky.braem@gmail.com>
2017-08-02 21:10:33 +0200

    Add a Fork me on Github image

--- src/site/components/site.vue
+++ src/site/components/site.vue
@@ -2,6 +2,9 @@
     <div class="uk-offcanvas-content">
         <div uk-sticky="media: 960" class="uk-navbar-container uk-sticky uk-light"
             style="background-image:linear-gradient(to left, #28a5f5, #1e87f0)">
+            <div style="float:right;margin:10px;">
+                <a href="https://github.com/fbraem/mqwebapp"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
+            </div>
             <div class="uk-container uk-container-expand ">
                 <nav class="uk-navbar">
                     <div class="uk-navbar-left">
